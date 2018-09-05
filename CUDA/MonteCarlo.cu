028424dddf0f2129d24f588f5b75b4ac4fb917ad
Liz <elizabeth.c.adelaide@gmail.com>
2018-09-05 12:06:47 -0400

    Create MonteCarlo.cu

---
+++ CUDA/MonteCarlo.cu
@@ -0,0 +1,154 @@
+/*
+
+Simple Monte Carlo Kernel Run
+
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <cuda_runtime.h>
+#include <cuda.h>
+#include <curand.h>
+
+#include <helper_cuda.h>
+
+/*CUDA Global constants*/
+
+__constant__ int N;
+__constant__ float T, r, sigma, rho, alpha, dt, con1, con2;
+
+/*Kernel routine*/
+__global__ void pathcalc(float *d_z, float *d_v) {
+	float s1, s2, y1, y2, payoff;
+	int ind;
+
+	//move array pointers to correct position
+
+	//version 1
+	ind = threadIdx.x + 2 * N*blockIdx.x*blockDim.x;
+
+	//path calculation
+	s1 = 1.0f;
+	s2 = 1.0f;
+
+	for (int n = 0; n < N; n++) {
+		y1 = d_z[ind];
+		ind += blockDim.x; //shift pointer to next element
+		s1 = s1 * (con1 + con2 * y1);
+		s2 = s2 * (con1 + con2 * y2);
+	}
+
+	//put payoff value into device array
+	if (fabs(s1 - 1.0f) < 0.1f && fabs(s2 - 1.0f) < 0.1f) payoff = exp(-r * T);
+
+	d_v[threadIdx.x + blockIdx.x*blockDim.x] = payoff;
+
+}
+
+//Main program
+
+int main(int argc, const char **argv) {
+
+	int NPATH = 9600, h_N = 100;
+	float h_T, h_r, h_sigma, h_rho, h_alpha, h_dt, h_con1, h_con2;
+	float *h_v, *d_v, *d_z;
+	double sum1, sum2;
+
+	//initilialize card
+	findCudaDevice(argc, argv);
+
+	//initialize cuda timing
+	float milli;
+	cudaEvent_t start, stop;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+
+	//Allocate memory on host and device
+	h_v = (float *)malloc(sizeof(float)*NPATH);
+
+	checkCudaErrors(cudaMalloc((void **)&d_v, sizeof(float)*NPATH));
+	checkCudaErrors(cudaMalloc((void **)&d_z, sizeof(float) * 2 * h_N*NPATH));
+
+	//define constants and transfer to GPU
+
+	h_T = 1.0f;
+	h_r = 0.05f;
+	h_sigma = 0.1f;
+	h_rho = 0.5f;
+	h_alpha = sqrt(1.0f - h_rho * h_rho);
+	h_dt = 1.0f / h_N;
+	h_con1 = 1.0f + h_r * h_dt;
+	h_con2 = sqrt(h_dt)*h_sigma;
+
+	//Gives error about typecasting? Just a IDE issue, not an actual error...
+	checkCudaErrors(cudaMemcpyToSymbol(N, &h_N, sizeof(h_N)));
+	checkCudaErrors(cudaMemcpyToSymbol(T, &h_T, sizeof(h_T)));
+	checkCudaErrors(cudaMemcpyToSymbol(r, &h_r, sizeof(h_r)));
+	checkCudaErrors(cudaMemcpyToSymbol(sigma, &h_sigma, sizeof(h_sigma)));
+	checkCudaErrors(cudaMemcpyToSymbol(rho, &h_rho, sizeof(h_rho)));
+	checkCudaErrors(cudaMemcpyToSymbol(alpha, &h_alpha, sizeof(h_alpha)));
+	checkCudaErrors(cudaMemcpyToSymbol(dt, &h_dt, sizeof(h_dt)));
+	checkCudaErrors(cudaMemcpyToSymbol(con1, &h_con1, sizeof(h_con1)));
+	checkCudaErrors(cudaMemcpyToSymbol(con2, &h_con2, sizeof(h_con2)));
+
+	//random number generation
+	cudaEventRecord(start);
+
+	curandGenerator_t gen;
+	checkCudaErrors(curandCreateGenerator(&gen, CURAND_RNG_PSEUDO_DEFAULT));
+	checkCudaErrors(curandSetPseudoRandomGeneratorSeed(gen, 1234ULL));
+	checkCudaErrors(curandGenerateNormal(gen, d_z, 2 * h_N*NPATH, 0.0f, 1.0f));
+
+	cudaEventRecord(stop);
+	cudaEventSynchronize(stop);
+	cudaEventElapsedTime(&milli, start, stop);
+
+	printf("CURAND normal RNG execution time (ms): %f, samples/sec: %e \n",
+		milli, 2.0*h_N*NPATH / (0.001*milli));
+
+	//Execute kernel and time:
+	cudaEventRecord(start);
+
+	pathcalc <<<NPATH / 64, 64 >>> (d_z, d_v);
+	getLastCudaError("Path calculation execution failed\n");
+
+	cudaEventRecord(stop);
+	cudaEventSynchronize(stop);
+	cudaEventElapsedTime(&milli, start, stop);
+
+	printf("Monte Carlo kernel execution time (ms): %f \n", milli);
+
+	//copy back results:
+
+	checkCudaErrors(cudaMemcpy(h_v, d_v, sizeof(float)*NPATH,
+		cudaMemcpyDeviceToHost));
+
+	//copy back results:
+	checkCudaErrors(cudaMemcpy(h_v, d_v, sizeof(float)*NPATH,
+		cudaMemcpyDeviceToHost));
+
+	//Get averaage on CPU (not parralelizable)
+	sum1 = 0.0;
+	sum2 = 0.0;
+	for (int i = 0; i < NPATH; i++) {
+		sum1 += h_v[i];
+		sum2 += h_v[i] * h_v[i];
+	}
+
+	printf("\nAverage value and standard deviation of error = %13.8f %13.8f\n\n",
+		sum1 / NPATH, sqrt((sum2 / NPATH - (sum1 / NPATH)*(sum1 / NPATH)) / NPATH));
+
+	//Tidying up!
+	checkCudaErrors(curandDestroyGenerator(gen));
+
+	//Release memory and exit cleanly
+	free(h_v);
+	checkCudaErrors(cudaFree(d_v));
+	checkCudaErrors(cudaFree(d_z));
+
+	//CUDA exit 
+	cudaDeviceReset();
+
+
+}
